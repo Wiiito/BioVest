db9635be8bbae71e365e9fba5517b0f61e15235e
Flavio Saldanha <tminemcraft123@gmail.com>
2025-07-10 21:17:33 -0300

    initial commit

---
+++ resources/js/components/app-logo.tsx
@@ -0,0 +1,14 @@
+import AppLogoIcon from './app-logo-icon';
+
+export default function AppLogo() {
+    return (
+        <>
+            <div>
+                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            </div>
+            <div className="ml-1 grid flex-1 text-left text-sm">
+                <span className="mb-0.5 truncate leading-tight font-semibold">BioVest</span>
+            </div>
+        </>
+    );
+}
